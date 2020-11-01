4363b8914f9e75483dff2b4b09bf346dd841fd5e
李玉 <595460399@qq.com>
2020-11-01 16:24:56 +0800

    數據統計功能完成

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -10,4 +10,14 @@ urlpatterns = [
     # 数据统计
     # 用户数量
     url(r'^statistical/total_count/$', statistical.UserCountView.as_view()),
+    #日增用户统计
+    url(r'^statistical/day_increment/$', statistical.UserDayCountView.as_view()),
+    #日活跃用户统计
+    url(r'^statistical/day_active/$', statistical.UserActiveCountView.as_view()),
+    #日下单用户量统计
+    url(r'^statistical/day_orders/$', statistical.UserOrderCountView.as_view()),
+    #月日增用户统计
+    url(r'^statistical/month_increment/$', statistical.UserMonthCountView.as_view()),
+    #日分类商品访问量
+    url(r'^statistical/goods_day_views/$', statistical.UserCountView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/statistical.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from datetime import date
+from datetime import date, timedelta
+
+from goods.models import GoodsVisitCount
+from meiduo_admin.serialziers.statistical import UserGoodsCountSerializer
 from users.models import User
+
+
 def UserCountView(APIView):
     """
     用户统计视图
@@ -11,12 +16,114 @@ def UserCountView(APIView):
     permission_class = [IsAdminUser]
 
     def get(self, request):
-        #获取当天时间
+        # 获取当天时间
         now_day = date.today()
-        #获取用户总量
+        # 获取用户总量
         count = User.objects.all().count()
-        #返回结果
+        # 返回结果
         return Response({
-            'date':now_day,
-            'count':count,
+            'date': now_day,
+            'count': count,
         })
+
+
+def UserDayCountView(APIView):
+    """
+    日增用户统计视图
+    """
+    # 权限指定
+    permission_class = [IsAdminUser]
+
+    def get(self, request):
+        # 获取当天时间
+        now_day = date.today()
+        # 获取用户日增量
+        count = User.objects.filter(date_joined__day__gte=now_day).count()
+        # 返回结果
+        return Response({
+            'date': now_day,
+            'count': count,
+        })
+
+
+class UserActiveCountView(APIView):
+    """
+    日活躍用戶統計
+    """
+    # 指定管理员权限
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        # 获取当前日期
+        now_date = date.today()
+        # 获取当日登录用户数量  last_login记录最后登录时间
+        count = User.objects.filter(last_login__gte=now_date).count()
+        return Response({
+            "count": count,
+            "date": now_date
+        })
+
+
+class UserOrderCountView(APIView):
+    '''
+    下單用戶統計
+    '''
+    # 指定管理员权限
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        # 获取当前日期
+        now_date = date.today()
+        # 获取当日下单用户数量  orders__create_time 订单创建时间
+        count = User.objects.filter(orders__create_time__gte=now_date).count()
+        return Response({
+            "count": count,
+            "date": now_date
+        })
+
+
+class UserMonthCountView(APIView):
+    """
+    統計每月每天的新增用戶量
+    從當天時間算起前一個月
+    """
+    # 指定管理员权限
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        # 获取当前日期
+        now_date = date.today()
+        # 获取一个月前日期
+        start_date = now_date - timedelta(29)
+        # 创建空列表保存每天的用户量
+        date_list = []
+
+        for i in range(30):
+            # 循环遍历获取当天日期
+            index_date = start_date + timedelta(days=i)
+            # 指定下一天日期
+            cur_date = start_date + timedelta(days=i + 1)
+            # 查询条件是大于当前日期index_date，小于明天日期的用户cur_date，得到当天用户量
+            count = User.objects.filter(date_joined__gte=index_date, date_joined__lt=cur_date).count()
+
+            date_list.append({
+                'count': count,
+                'date': index_date
+            })
+
+            return Response(date_list)
+
+
+class UserGoodsCountView(APIView):
+    '''
+    獲取當天訪問量數量
+    '''
+    def get(self, request):
+        # 获取当天日期
+        now_date = date.today()
+        # 获取当天访问的商品分类数量信息
+        goods = GoodsVisitCount.objects.filter(date=now_date)
+        # 序列化返回分类数量
+        ser = UserGoodsCountSerializer(goods, many=True)
+
+        return Response(ser.data)
